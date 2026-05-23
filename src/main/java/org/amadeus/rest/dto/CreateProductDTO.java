175565db17dcdfb07f1d1828420223fd0fa14f76
Miguel Rico <miguel35697@hotmail.com>
2026-05-23 08:30:16 -0500

    fix: typo

--- src/main/java/org/amadeus/rest/dto/CreateProductDTO.java
+++ src/main/java/org/amadeus/rest/dto/CreateProductDTO.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.PositiveOrZero;
 
 public record CreateProductDTO(
         @NotBlank(message = "product name is required") String name,
-        @NotNull(message = "product price is required") @Positive(message = "productt price must be greater than 0") Double price,
+        @NotNull(message = "product price is required") @Positive(message = "product price must be greater than 0") Double price,
         @NotBlank(message = "product description is required") String description,
         @NotNull(message = "product quantity is required") @PositiveOrZero(message = "product quantity must be 0 or greater") Integer quantity,
         @NotBlank(message = "product tags are required") String tags
